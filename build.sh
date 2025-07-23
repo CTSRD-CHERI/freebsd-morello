e25614489096aa50119949b9d9448c5d18a3462e
Ruslan Bukin <br@bsdpad.com>
2025-07-23 12:45:33 +0100

    make binary

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ else
 fi
 
 make -j24 TARGET=riscv KERN${A}=GENERIC buildkernel || exit 1
+
+riscv64-unknown-freebsd15.0-objcopy -O binary /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel.bin
