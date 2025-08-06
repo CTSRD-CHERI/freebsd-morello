84bce6283f349cc4e799aa5e0751975ca3d3c1bc
Ruslan Bukin <br@bsdpad.com>
2025-08-06 14:41:50 +0100

    add echo

--- build.sh
+++ build.sh
@@ -10,3 +10,5 @@ make -j24 TARGET=riscv KERN${A}=GENERIC buildkernel || exit 1
 riscv64-unknown-freebsd15.0-objcopy -O binary /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel.bin
 
 scp /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/usr.sbin/hwc/hwc 10.4.0.2:~/
+
+echo "/usr/bin/cpuset -l 0 ./hwc -f qemu /usr/bin/uname"
