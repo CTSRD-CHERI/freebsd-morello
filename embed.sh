53f4de61942381573ff5cb682f428936c3e092b1
Ruslan Bukin <br@bsdpad.com>
2025-08-06 14:44:00 +0100

    whitespace

--- embed.sh
+++ embed.sh
@@ -1,3 +1,3 @@
-sh ./sys/tools/embed_mfs.sh /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /home/br/dev/freebsd/mdroot.img 
+sh ./sys/tools/embed_mfs.sh /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /home/br/dev/freebsd/mdroot.img
 
 cp /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /home/br/dev/cva6-tools/gdb-freebsd/kernel_only
